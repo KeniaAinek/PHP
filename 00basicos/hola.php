9b712025c2c387acb980da339026920c844d569a
Kenia Castro <keniad.ctorres@gmail.com>
2024-06-19 19:35:55 -0700

    Archivo hola.php actulizado

--- 00basicos/hola.php
+++ 00basicos/hola.php
@@ -7,9 +7,32 @@ de varias lineas */
 //imprimir en pantala
 echo("Hola mundo");
 
-echo("</br>Hola cruel mundo<br/><h1>estoy apreniendo PHP</h1>")
+echo("</br>Hola cruel mundo<br/><h1>estoy apreniendo PHP</h1>");
 
 //variables
+$nombre = "Kenia";
+$Nombre = "Valentina";
+//Concatenacion de cadenas y variables
+echo $nombre." & ".$Nombre;
+echo "<br/>";
+$num1 = 5;
+$num2 = 78;
+$suma = $num1 + $num2;
+echo $suma;
+echo "<br/>";
+echo "La variables \$suma tiene el valor de $suma </br>";
 
+//Estructuras
+$modulo = $num2 % 2;
+if($modulo == 0){
+    echo "El n&uacutemero es Par";
+}else{
+    echo "El n&uacutemero es impar";
+}
+echo "</br>";
+
+for($i = 0; $i <= 10; $i++){
+    echo $i."<br/>";
+}
 
 ?>
