1e9156accf7cf85b868950717cc2a0edc4634c41
Kenia Castro <keniad.ctorres@gmail.com>
2024-06-27 20:07:25 -0700

    04Cookies agregado

---
+++ 04Cookies/pedir-idioma.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8"/>
+    <title>Cookies en PHP</title>
+</head>
+<body>
+    <a href="grabar-cookie.php?idioma=es">Versi&oacute;n en espa&ntilde;ol</a>
+</br></br>
+<a href="grabar-cookie.php?idioma=es">Versi&oacute;n en ingl&eacute;s</a>
+    
+</body>
