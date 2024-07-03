ae77557215c7ab48f2b99cbf4fe9e29edc02cf3d
k.castro <k.castro@mefasa.org>
2024-07-03 11:06:16 -0700

    Distribucion de codigo en carpetas views y partials

--- Demo/index.php
+++ Demo/index.php
@@ -1,40 +1,6 @@
 <?php
- $books = [
-           [
-            'name' => 'Do Androids Dream of Electric Sheep',
-            'author' => 'Philip K. Dick',
-            'releaseYear' => 1968,
-            'purchaseUrl' => 'http://example.com'
-           ],
-           [
-            'name' => 'The Langoliers',
-            'author' => 'Andy Weir',
-            'releaseYear' => 2021,
-            'purchaseUrl' => 'http://example.com'
-           ],
-           [
-            'name' => 'Hail Mary',
-            'author' => 'Philip K. Dick',
-            'releaseYear' => 2011,
-            'purchaseUrl' => 'http://example.com'
-           ],
-          ];
 
-          /* function filter($items, $fn){
-            $filterItems = [];
+  $heading = "Home";
 
-            foreach ($items as $item){
-                if($fn($item)){
-                    $filterItems[] = $item;
-                }
-            }
-            return $filterItems;
-          } */
-
-          $filterBooks = array_filter($books, function($book){
-            return $book['releaseYear'] < 2000;
-          });
-
-          include "index.view.php";
-          
+  require "views/index.view.php";  
 ?>
---
+++ Demo/views/index.view.php
@@ -0,0 +1,12 @@
+
+<?php require ("../Demo/views/partials/head.php") ?>
+   <?php require ("../Demo/views/partials/nav.php") ?>
+   <?php require ("../Demo/views/partials/banner.php") ?>
+
+  <main>
+    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+      <p>Now you are on the home page.</p>
+    </div>
+  </main>
+
+  <?php require "../Demo/views/partials/footer.php"; ?>
