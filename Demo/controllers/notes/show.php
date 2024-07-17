a5d64edbcefebd1113e255ede69f1d8b586bd94e
Kenia Castro <keniad.ctorres@gmail.com>
2024-07-16 18:10:20 -0700

    edit note updated method

--- Demo/controllers/notes/show.php
+++ Demo/controllers/notes/show.php
@@ -1,23 +1,17 @@
 <?php
 
+use Core\App;
 use Core\Database;
 
-$config = require base_path('config.php');
-
-$db = new Database($config['database']);
+$db = App::resolve(Database::class);
 
 $currentUserId = 1;
 
-$note = $db->query('SELECT * from notes where id = :id', [
-    'id' => $_GET['id']
-])->findOrFail();
+$note = $db->query('SELECT * FROM notes WHERE id = :id', ['id' => $_GET['id']])->findOrFail();
 
-authorize($note['user_id'] == $currentUserId);
+authorize($note['user_id'] === $currentUserId);
 
 view("notes/show.view.php", [
     'heading' => 'Note',
     'note' => $note
 ]);
-
-
-?>
