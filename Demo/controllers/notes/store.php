456476da63a020b108412e26fa0448ca9847e75e
Kenia Castro <keniad.ctorres@gmail.com>
2024-07-21 20:57:43 -0700

    changes on destroy and store controllers

---
+++ Demo/controllers/notes/store.php
@@ -0,0 +1,27 @@
+<?php 
+use Core\Validator;
+use Core\Database;
+$config = require base_path('config.php');
+
+$db = new Database($config['database']);
+
+$errors = [];
+
+    if(!Validator::string($_POST['body'], 1, 1000))
+    {
+        $errors['body'] = 'A of no more than 1000 characters is required.';
+    }
+
+    if(! empty($errors)){
+        return view("notes/create.view.php", [
+            'heading' => 'Create Note',
+            'errors' => $errors
+        ]);
+    }
+
+        
+    $db -> query('INSERT INTO notes (body, user_id) VALUES (:body, :user_id)',  [ 'body' => $_POST['body'], 'user_id' => 1 ]);
+
+    header('location: /PHP/Demo/notes');
+    die();
+    
