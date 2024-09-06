e5399b8bdb9fcef3804d3f8457d5c56bce819274
k.castro <k.castro@mefasa.org>
2024-09-05 17:06:04 -0700

    login form started

--- Demos/resources/views/jobs/edit.blade.php
+++ Demos/resources/views/jobs/edit.blade.php
@@ -16,7 +16,7 @@
             <div class="mt-2">
               <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
                 <span class="flex select-none items-center pl-3 text-gray-500 sm:text-sm"></span>
-                <input type="text" name="title" id="title" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="Shift Leader" required>
+                <input type="text" name="title" id="title" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="Shift Leader" value="{{$job->title}}" required>
               </div>
 
               @error('title')
@@ -30,7 +30,7 @@
         <label for="salary" class="block text-sm font-medium leading-6 text-gray-900">Salary</label>
         <div class="mt-2">
           <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-            <input type="text" name="salary" id="salary" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="$50,000.00 per year">
+            <input type="text" name="salary" id="salary" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="$50,000.00 USD per year" value="{{$job->salary}}" required>
           </div>
           @error('salary')
                 <p class="text-xs text-red-500 font-semibold">{{$message}}</p>
@@ -40,16 +40,16 @@
   
     <div class="flex items-center justify-between gap-x-6">
       <div class="mt-6 items-center ">
-        <button type="button" form="delete" class="text-md font-semibold leading-6 text-red-500">Delete</button>
+        <button form="delete" class="text-md font-semibold leading-6 text-red-500">Delete</button>
       </div>
       <div class="mt-6 items-center">
-      <button type="button" class="text-sm font-semibold leading-6 text-gray-900 pr-3">Cancel</button>
-      <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+      <a href="/jobs/{{$job->id}}" class="text-sm font-semibold leading-6 text-gray-900 pr-3">Cancel</a>
+      <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Update</button>
     </div>
   </div>
   </form>
 
-  <form action="/jobs/{{$job->id}}" method="POST" id="delete" class="hidden">
+  <form method="POST" action="/jobs/{{$job->id}}" class="hidden" id="delete" >
     @csrf
     @method('DELETE')
   </form>
--- Demos/routes/web.php
+++ Demos/routes/web.php
@@ -1,74 +1,20 @@
 <?php
 use App\Models\Job;
+use App\Http\Controllers\JobController; 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::view('/', 'home');
+Route::view('/contact', 'contact');
 
-Route::get('/jobs', function () {
-    $jobs = Job::with('employer')->latest()->paginate(3);
-    
-    return view('jobs.index', [
-        'jobs' => $jobs
-    ]);
-});
+/* Route::controller(JobController::class)->group(function (){
+    Route::get('/jobs', 'index');
+    Route::get('/jobs/create', 'create');
+    Route::get('/jobs/{job}', 'show');
+    Route::post('/jobs', 'store');
+    Route::get('/jobs/{job}/edit', 'edit');
+    Route::patch('/jobs/{job}', 'update');
+    Route::delete('/jobs/{job}', 'destroy');
+}); */
 
-Route::get('/jobs/create', function () {
-    return view('jobs.create');
-});
+Route::resource('jobs', JobController::class);
 
-Route::get('/jobs/{id}', function ($id) {
-    $job = Job::find($id);
-    return view('jobs.show', ['job' => $job]);
-});
-
-Route::post('/jobs', function () {
-    // Validation 
-    request()->validate([
-        'title' => ['required', 'min:3'],
-        'salary' => ['required'],
-    ]);
-
-    Job::create([
-        'title' => request('title'),
-        'salary' => request('salary'),
-        'employer_id' => 1
-    ]);
-
-    return redirect('/jobs');
-});
-
-Route::get('/jobs/{id}/edit', function ($id) {
-    $job = Job::find($id);
-    return view('jobs.edit', ['job' => $job]);
-});
-
-Route::patch('/jobs/{id}', function ($id) {
-    //Validate 
-    request()->validate([
-        'title' => ['required', 'min:3'],
-        'salary' => ['required'],
-    ]);
-    //autorize (On hold...)
-    
-    $job = Job::findOrFail($id);
-    
-    $job->update([
-        'title' => request('title'),
-        'salary' => request('salary'),
-    ]);
-    
-    return redirect('/jobs/' . $job->id);
-});
-
-Route::delete('/jobs/{id}', function ($id) {
-    //autorize (On hold...)
-    Job::findOrFail($id)->delete();
-
-    return redirect('/jobs');
-});
-
-Route::get('/contact', function () {
-    return view('contact');
-});
