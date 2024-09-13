d831f4a5b14c1a19f097b918f6943ed83cd5c61d
k.castro <k.castro@mefasa.org>
2024-09-13 13:44:40 -0700

    proyecto final pixel-positions iniciado migraciones creadas

---
+++ pixel-positions/resources/views/components/job-card-wide.blade.php
@@ -0,0 +1,25 @@
+<x-panel class="flex gap-x-6">
+
+    <div>
+        <x-employer-logo />
+    </div>
+
+    <div class="flex-1">
+
+        <a href="#" class="self-start text-sm text-gray-400">Laracasts</a>
+        
+            <h3 class="font-bold text-xl mt-3 group-hover:text-blue-800 transitions-colors duration-300">Video Producer</h3>
+            <p class="text-sm text-gray-400">Full Time - From $60,000</p>
+        
+    </div>
+
+    <div>
+        <div>
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+        </div>
+        
+        
+    </div>
+</x-panel>
--- pixel-positions/resources/views/components/layout.blade.php
+++ pixel-positions/resources/views/components/layout.blade.php
@@ -6,8 +6,12 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Pixel Positions</title>
     @vite(['resources/js/app.js'])
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Hanken+Grotesk:ital,wght@400;500;600&display=swap" rel="stylesheet">
 </head>
-<body class="bg-black text-white">
+
+<body class="bg-black text-white font-hanken-grotesk">
 
     <div class="px-10">
         <nav class="flex justify-between item-center py-4 border-b border-white/10">
